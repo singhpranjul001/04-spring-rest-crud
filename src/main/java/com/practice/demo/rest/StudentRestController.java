70af89c820172861ef3c250fafaaef2d11a8193d
Pranjul Singh <singhpranjul001@gmail.com>
2024-04-25 13:27:18 +0530

    Learned about Exception Handling.

--- src/main/java/com/practice/demo/rest/StudentRestController.java
+++ src/main/java/com/practice/demo/rest/StudentRestController.java
@@ -2,10 +2,9 @@ package com.practice.demo.rest;
 
 import com.practice.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,6 +36,43 @@ public class StudentRestController {
 
     @GetMapping("/students/{studentId}")
     public Student getStudent(@PathVariable int studentId){
+        //check the studentId against the list size
+
+        if((studentId>=theStudents.size() || studentId<0)){
+            throw new StudentNotFoundException("Student Id " + studentId + " not found");
+        }
         return theStudents.get(studentId);
+    }
+
+    //Add an exception handler using @ExceptionHandler
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
+
+        //create a StudentErrorResponse
+        StudentErrorResponse error= new StudentErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        //return ResponseEntity
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    //add another exception handler.. to catch any exception
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
+
+        //create a StudentErrorResponse
+        StudentErrorResponse error= new StudentErrorResponse();
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        // to set custom message just write your custom message
+        // error.setMessage("Characters are not allowed");
+
+        error.setTimeStamp(System.currentTimeMillis());
+
+        //return ResponseEntity
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
